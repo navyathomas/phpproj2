1e8f801675e3b74727627f1bab63ac227be2f70d
navyathomas <navyathankam15@gmail.com>
2018-12-12 12:19:40 +0530

    project upload

---
+++ Login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            .myclass{
+                background: pink;
+            }
+            .myvalue{
+                background: lightblue;
+            }
+        </style>
+    </head>
+    <body class="myclass" >
+       
+   
+    <table>
+        <tr>
+            <td>username</td>
+            <td><input type="text"/></td>
+        </tr>  
+        <tr>
+            <td>password</td>
+            <td><input type="password"/></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input class="myvalue" type="submit" value="login"/></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><a href="./index.php">go to register page</a></td>
+        </tr>
+        
+        
+        
+    </table>
+         </body>
+</html>
---
+++ index.php
@@ -0,0 +1,69 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Web Page</title>
+        <style>
+            .myBody{
+                background: pink;
+            } 
+            .myButton{
+                background: red;
+            }
+        </style>
+    </head>
+    <body class="myBody">
+        <table>
+            <tr>
+                <td>name</td>
+                <td> <input type="text"/></td>
+            </tr>
+            <tr>
+                <td>rollno</td>
+                <td> <input type="number"/></td>
+            </tr>
+            <tr>
+                <td>admission no</td>
+                <td> <input type="text"/></td>
+            </tr>
+            <tr>
+                <td>college</td>
+                <td> <input type="text"/></td>
+            </tr>
+            <tr>
+                <td>mobile no</td>
+                <td> <input type="text"/></td>
+            </tr>
+            <tr>
+                <td>place</td>
+                <td> <input type="text"/></td>
+            </tr>
+            <tr>
+                <td>date of birth</td>
+                <td><input type="date"/></td>
+            </tr>
+            <tr>
+                <td>username</td>
+                <td><input type="text"/></td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td><input type="password"/></td>
+            </tr>
+            <tr>
+                <td>confirm password</td>
+                <td><input type="password"/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input class="myBody"type="submit" value="Register"/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><a href="./Login.php">go to login page</a></td>
+            </tr>
+        </table>
+   
+    
+    </body>
+</html>
